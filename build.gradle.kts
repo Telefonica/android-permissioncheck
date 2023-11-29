2090f7615c316db16e0749aa5220a927eadc4789
David González <david.gonzalezcolorado@telefonica.com>
2023-11-29 13:23:39 +0100

    ANDROID-13765 Return comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 		classpath("com.android.tools.build:gradle:8.1.4")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
 
-		classpath("com.telefonica:manifestcheck:+") // Uncomment to use the sample
+//		classpath("com.telefonica:manifestcheck:+") // Uncomment to use the sample
 	}
 }
 
